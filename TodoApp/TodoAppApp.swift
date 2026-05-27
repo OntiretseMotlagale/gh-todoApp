99be76c4b4eb28b426477b271320b28fc9b0929f
ontiretsemmotlagale <ontiretsemotlagale@gmail.com>
2026-05-27 22:23:07 +0200

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Ontiretse Motlagale on 2026/05/27.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

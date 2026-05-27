2b4fab3d6f7c26751ae5fa09622b889fc614e002
ontiretsemmotlagale <ontiretsemotlagale@gmail.com>
2026-05-27 23:07:42 +0200

    Updated the yml file

--- TodoAppTests/TodoAppTests.swift
+++ TodoAppTests/TodoAppTests.swift
@@ -1,36 +1,56 @@
-//
-//  TodoAppTests.swift
-//  TodoAppTests
-//
-//  Created by Ontiretse Motlagale on 2026/05/27.
-//
-
-import Testing
-
 import XCTest
 @testable import TodoApp
 
 final class TodoViewModelTests: XCTestCase {
 
-    func test_addTodo() {
+    func test_addTodo_shouldIncreaseCount() {
         let vm = TodoViewModel()
 
-        vm.addTodo(title: "Test")
+        vm.addTodo(title: "Buy milk")
 
         XCTAssertEqual(vm.todos.count, 1)
-        XCTAssertEqual(vm.todos.first?.title, "Test")
     }
 
-    func test_toggleTodo() {
+    func test_addTodo_shouldStoreCorrectTitle() {
         let vm = TodoViewModel()
-        vm.addTodo(title: "Test")
-        XCTAssertFalse(vm.todos.isEmpty)
 
+        vm.addTodo(title: "Buy milk")
+
+        XCTAssertEqual(vm.todos.first?.title, "Buy milk")
+    }
+
+    func test_toggleTodo_shouldMarkTodoAsDone() {
+        let vm = TodoViewModel()
+
+        vm.addTodo(title: "Study Swift")
         let id = vm.todos.first!.id
+
         vm.toggleTodo(id: id)
 
         XCTAssertTrue(vm.todos.first!.isDone)
+    }
 
-        XCTAssertTrue(vm.todos.first!.isDone)
+    func test_toggleTodo_shouldToggleBackToNotDone() {
+        let vm = TodoViewModel()
+
+        vm.addTodo(title: "Study Swift")
+        let id = vm.todos.first!.id
+
+        vm.toggleTodo(id: id)
+        vm.toggleTodo(id: id)
+
+        XCTAssertFalse(vm.todos.first!.isDone)
+    }
+
+    func test_toggleTodo_withInvalidID_shouldNotCrash() {
+        let vm = TodoViewModel()
+
+        vm.addTodo(title: "Study Swift")
+
+        let randomID = UUID()
+        vm.toggleTodo(id: randomID)
+
+        XCTAssertEqual(vm.todos.count, 1)
+        XCTAssertFalse(vm.todos.first!.isDone)
     }
 }
